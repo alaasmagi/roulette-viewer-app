42ebb1034c303667020810ee0aa2c4d511827c1c
Aleksander Laasmägi <allaas@taltech.ee>
2025-04-01 23:23:20 +0300

    Initial commit

---
+++ WebApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WebApp.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ WebApp/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace WebApp.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
